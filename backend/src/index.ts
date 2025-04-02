a8cf2ac43d6654c16c50adfce4446f68f8da3add
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-04-02 10:12:03 +0530

    Initialized backend

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
+const app: Application = express();
+const PORT = process.env.PORT || 7000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hey coming from Backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
