d59c67fd3fdab915f5d42e73eafbf8e145c710e6
lakshay-2411 <lakshaynandwani06@gmail.com>
2025-04-03 23:19:36 +0530

    Added mail service

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,30 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import path from "path";
+import ejs from "ejs";
+import { fileURLToPath } from "url";
+import { sendEmail } from "./config/mail.js";
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hey coming from Backend!");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Set view engine to EJS
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/", async (req: Request, res: Response) => {
+  try {
+    const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {
+      name: "Lakshay Nandwani",
+    });
+    await sendEmail("gifixan327@motivue.com", "Testing SMTP", html);
+    res.json({ msg: "Email sent successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(PORT, () => {
